22821531b92379f4e3711e33bcb59676c7af14bc
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-09-05 03:12:22 +0530

    ✅ Added registration tests #3

--- server/tests/test_server.py
+++ server/tests/test_server.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import requests
-
+from base import school_register
+import json
 
 class TestServerBoot(TestCase):
     @classmethod
@@ -14,3 +15,33 @@ class TestServerBoot(TestCase):
     def test_server(self):
         status = self.request.get(self.base_url + "apis/healthcheck")
         self.assertEqual(200, status.status_code)
+
+
+    def test_register(self):
+        doc = school_register()
+        headers = {'Content-type': 'application/json'}
+        status = self.request.post(self.base_url + "apis/register", data=json.dumps(doc), headers=headers)
+        self.assertEqual(status.status_code, 201)
+
+        doc["units"] = 51
+        status = self.request.post(self.base_url + "apis/register", data=json.dumps(doc), headers=headers)
+        self.assertEqual(status.status_code, 400)
+
+        doc = school_register()
+        doc.pop("school_name")
+        status = self.request.post(self.base_url + "apis/register", data=json.dumps(doc), headers=headers)
+        self.assertEqual(status.status_code, 400)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        import pymongo
+        import os
+        from dotenv import load_dotenv
+
+        load_dotenv()
+
+        client = pymongo.MongoClient(os.getenv("MONGO_URI"))
+        db = client[os.getenv("TESTDB")]
+
+        db.drop_collection("schools")
+        db.drop_collection("units")
