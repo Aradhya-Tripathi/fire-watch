5c711d289da12c68874c042b49ee04ca82f2c2d8
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-09-09 06:48:18 +0530

    🚩 updated token handle

--- server/apis/urls.py
+++ server/apis/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("healthcheck", HealthCheck.as_view()),
     path("register", Register.as_view()),
     path("login", Login.as_view()),
-    path("protected", ProtectedView.as_view())
+    path("protected", ProtectedView.as_view()),
 ]
