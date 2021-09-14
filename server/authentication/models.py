7ae62a38090d4bf6cb90915c0caef0aef89d0ceb
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-09-14 18:10:25 +0530

    feat: secured sos and socket routes

--- server/authentication/models.py
+++ server/authentication/models.py
@@ -16,3 +16,7 @@ class AuthModel:
         if documents:
             return True
         raise InvalidUid(f"No document with unit with Id {unit_id} found")
+
+    def id_from_school(self, school_name: str):
+        school = self.db.schools.find_one({"school_name": school_name})
+        return school["unit_id"]
