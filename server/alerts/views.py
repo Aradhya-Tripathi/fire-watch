bb45fad35da7b95135952b2d57af6e5c1089cae3
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-09-14 18:09:12 +0530

    feat: implemented alert socket

--- server/alerts/views.py
+++ server/alerts/views.py
@@ -3,23 +3,24 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
 
-
 class Alert(JsonWebsocketConsumer):
     group_name = "Alert"
 
     def connect(self):
-        if isinstance(self.scope.get("error"), str):
+        if "error" in self.scope:
             self.close()
-            return
+            return None
 
         self.accept()
+        self.channel_layer = get_channel_layer()
         self.add_to_group()
 
+    def send_alert(self, content):
+        self.send_json(content["content"])
+
     def add_to_group(self):
-        channel_layer = get_channel_layer()
-        self.group_id = self.group_name + str(self.scope["current"]["exp"])
-        print(self.group_id)
-        async_to_sync(channel_layer.group_add)(self.group_id, self.channel_name)
+        self.group_id = self.group_name + str(self.scope["current"])
+        async_to_sync(self.channel_layer.group_add)(self.group_id, self.channel_name)
 
     def disconnect(self, code):
         super().disconnect(code)
