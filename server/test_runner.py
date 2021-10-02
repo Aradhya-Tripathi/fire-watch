4ad9a4238e9a8f0bf9a7fac11a2563a6a66fd94a
Aradhya-Tripathi <aradhyatripathi51@gmail.com>
2021-10-03 04:17:19 +0530

    wip: making better options for test client

--- server/test_runner.py
+++ server/test_runner.py
@@ -8,19 +8,34 @@ import argparse
 def configure_options():
     parser = argparse.ArgumentParser()
     parser.add_argument("--unit-tests", nargs=1, default=0, type=int)
+    parser.add_argument("--allow-both", nargs=1, default=[0], type=int)
     return parser
+
+
+def get_unittests(suite):
+    suite.addTest(unittest.makeSuite(test_auth_model.TestAuthModel))
+    suite.addTest(unittest.makeSuite(test_jwt.TestJwt))
+
+
+def get_server_tests(suite):
+    suite.addTest(unittest.makeSuite(test_login.TestServer))
+    suite.addTest(unittest.makeSuite(test_units.TestUnit))
 
 
 def main():
     suite = unittest.TestSuite()
     options = configure_options().parse_args()
-    if options.unit_tests:
-        # Add module level unit tests to test suite
-        suite.addTest(unittest.makeSuite(test_auth_model.TestAuthModel))
-        suite.addTest(unittest.makeSuite(test_jwt.TestJwt))
 
-    suite.addTest(unittest.makeSuite(test_login.TestServer))
-    suite.addTest(unittest.makeSuite(test_units.TestUnit))
+    if not options.allow_both[0]:
+        if options.unit_tests[0]:
+            # Add module level unit tests to test suite
+            get_unittests(suite)
+        else:
+            get_server_tests(suite)
+
+    else:
+        get_unittests(suite)
+        get_server_tests(suite)
 
     output = unittest.TextTestRunner().run(suite)
     if output.errors or output.failures:
