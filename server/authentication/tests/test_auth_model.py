543a4a0f5fdd334c6496b8a03cdb9a8c2d308d5e
Aradhya <aradhyatripathi51@gmail.com>
2021-12-08 15:54:26 +0530

    fix(tests): fixed test cases

--- server/authentication/tests/test_auth_model.py
+++ server/authentication/tests/test_auth_model.py
@@ -15,11 +15,11 @@ class TestAuthModel(unittest.TestCase):
         self.auth_model.db.units.insert_one({"unit_id": "randomunit_id"})
 
     def test_validate_token(self):
-        self.assertTrue(utils.validate_request("randomunit_id"))
+        self.assertTrue(utils.validate_unit_id("randomunit_id"))
 
     def test_invalid_token(self):
         with self.assertRaises(InvalidUid) as error:
-            utils.validate_request("invalidunit_id")
+            utils.validate_unit_id("invalidunit_id")
 
     def tearDown(self) -> None:
         self.auth_model.db.units.remove({"unit_id": "randomunit_id"})
