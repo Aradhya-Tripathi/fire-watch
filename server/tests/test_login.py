ffcdc640074b9d40320e92e5d1aec3b5a3746068
Aradhya <aradhyatripathi51@gmail.com>
2021-12-13 23:10:02 +0530

    refactor(tests): using setup method

--- server/tests/test_login.py
+++ server/tests/test_login.py
@@ -3,9 +3,10 @@ import json
 
 
 class TestAuthentication(CustomTestCase):
-    def test_register(self):
+    def setUp(self) -> None:
         self.clear_all()
 
+    def test_register(self):
         doc = self.user_register()
         headers = {"Content-type": "application/json"}
         doc["units"] = 51
@@ -21,7 +22,6 @@ class TestAuthentication(CustomTestCase):
         self.assertEqual(status.status_code, 400)
 
     def test_login(self):
-        self.clear_all()
         doc = self.user_register()
         headers = {"Content-type": "application/json"}
         status = self.request.post(
@@ -56,6 +56,25 @@ class TestAuthentication(CustomTestCase):
         )
 
         self.assertEqual(status.status_code, 401)
+
+    def test_protected_route(self):
+        headers = {"Content-type": "application/json"}
+        status = self.request.get(self.base_url + "apis/protected", headers=headers)
+        self.assertEqual(status.status_code, 403)
+        doc = self.user_register()
+        headers = {"Content-type": "application/json"}
+        status = self.request.post(
+            self.base_url + "apis/register", data=json.dumps(doc), headers=headers
+        )
+        self.assertEqual(status.status_code, 201)
+
+        creds = {"password": doc["password"], "email": doc["email"]}
+        tokens = self.request.post(
+            self.base_url + "apis/login", data=json.dumps(creds), headers=headers
+        )
+        headers.update({"Authorization": f"Bearer {tokens.json()['access_token']}"})
+        status = self.request.get(self.base_url + "apis/protected", headers=headers)
+        self.assertEqual(status.status_code, 200)
 
     def tearDown(self) -> None:
         self.clear_all()
