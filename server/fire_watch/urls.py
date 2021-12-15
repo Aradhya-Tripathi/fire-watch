15e9af1e22d1d85c4bae6cd0bb5f8776646f6d2a
Aradhya <aradhyatripathi51@gmail.com>
2021-12-16 00:08:13 +0530

    ✨ Remodeled urls

--- server/fire_watch/urls.py
+++ server/fire_watch/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from .views import health_check
 
 urlpatterns = [
-    path("apis/", include("apis.urls")),
+    path("", include("apis.routes.open_urls")),
     path("health-check", health_check),
+    path("user/", include("apis.routes.protected_urls")),
 ]
