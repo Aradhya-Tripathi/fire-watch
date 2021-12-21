ecc37110c1a506eecd8fea7be34b2e408a04654b
Aradhya <aradhyatripathi51@gmail.com>
2021-12-21 13:39:50 +0530

    ♻️ Refactored urls

--- server/fire_watch/urls.py
+++ server/fire_watch/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", include("apis.routes.open_urls")),
     path("health-check", health_check),
     path("user/", include("apis.routes.protected_urls")),
+    path("admin/", include("admin.routes.protected_urls"))
 ]
