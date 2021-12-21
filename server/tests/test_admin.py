89540021c0da4223caa6cdde58702e2c44d49fcd
Aradhya <aradhyatripathi51@gmail.com>
2021-12-22 01:10:08 +0530

    ✅ Added admin tests

--- server/tests/test_admin.py
+++ server/tests/test_admin.py
@@ -1,9 +1,20 @@
+import json
+from hashlib import sha256
+
 from .base import CustomTestCase
 
 
 class TestAdmin(CustomTestCase):
     def setUp(self) -> None:
         self.clear_all()
+
+    def create_admin(self):
+        self.db.AdminCredentials.insert_one(
+            {
+                "email": "admin@example.com",
+                "password": sha256("adminpassword".encode()).hexdigest(),
+            }
+        )
 
     def test_admin_response(self):
         user_creds = self.user_login()
@@ -14,10 +25,32 @@ class TestAdmin(CustomTestCase):
         )
         self.assertEqual(user_response.status_code, 200)
         # Assert no login through user jwt
-        admin_response = self.request.get(self.base_url + "admin", headers=headers)
+        admin_response = self.request.get(
+            self.base_url + "admin/details", headers=headers
+        )
         self.assertEqual(admin_response.status_code, 403)
-        admin_response = self.request.get(self.base_url + "admin", headers=self.headers)
+        admin_response = self.request.get(
+            self.base_url + "admin/details", headers=self.headers
+        )
         self.assertEqual(admin_response.status_code, 403)
+
+    def test_admin_login(self):
+        self.create_admin()
+        admin_response = self.request.post(
+            self.base_url + "admin/details",
+            data=json.dumps(
+                {"email": "admin@example.com", "password": "adminpassword"}
+            ),
+            headers=self.headers,
+        )
+        self.assertEqual(admin_response.status_code, 200)
+        admin_creds = admin_response.json()["access_token"]
+        headers = self.headers.copy()
+        headers.update({"Authorization": f"Bearer {admin_creds}"})
+        admin_response = self.request.get(
+            self.base_url + "admin/details", headers=headers
+        )
+        self.assertEqual(admin_response.status_code, 200)
 
     def tearDown(self) -> None:
         self.clear_all()
